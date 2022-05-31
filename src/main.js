d362a8ea93dad3f496a14011d7817a48a761cc7a
Paulie3 <pavel.hrdina.ml@seznam.cz>
2022-05-31 09:05:51 +0200

    added function that hides text

--- src/main.js
+++ src/main.js
@@ -2,8 +2,20 @@ import "../dist/output.css";
 
 // Globalní proměné
 const range = document.getElementById("range");
+const textKterySeSchova = document.getElementById("text");
+const button = document.getElementById("schovatText");
+
+// Schová text a tlačítko
+
+button.addEventListener("click", () => {
+  textKterySeSchova.innerHTML = "";
+  button.classList = "";
+  button.innerHTML = "";
+});
 
 // aktivuje se při změně člísla v inputu
 range.addEventListener("change", () => {
   return (document.getElementById("delkaHesla").innerHTML = range.value);
 });
+
+// Vygeneruje náhodné heslo
